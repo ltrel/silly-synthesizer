a0710e31fa0cdccd2ab233c589f1837f68e30c22
ltrel <79512656+ltrel@users.noreply.github.com>
2021-08-15 15:54:41 +1000

    Play first 8 bars of Bach BWV 847

--- main.cpp
+++ main.cpp
@@ -4,40 +4,212 @@
 #include "synthesizer.h"
 #include "utils.h"
 
+std::vector<std::vector<std::vector<double>>> bachPrelude(Synthesizer &synth);
+
 int main(int argc, char *argv[])
 {
   int sampleRate{44100};
   int numChannels{2};
   int bitsPerSample{24};
 
-  double crotchet{0.8};
-
-  std::vector<std::vector<double>> audioData(numChannels, std::vector<double>{});
-
   SynthOptions options;
-  options.baseVolume = 1/6.0;
+  options.baseVolume = 1 / 6.0;
   Synthesizer synth{sampleRate, numChannels, options};
 
-  // Notes to the first phrase of Ode to Joy
-  synth.renderNote(audioData, "e4", crotchet);
-  synth.renderNote(audioData, "e4", crotchet);
-  synth.renderNote(audioData, "f4", crotchet);
-  synth.renderNote(audioData, "g4", crotchet);
-  synth.renderNote(audioData, "g4", crotchet);
-  synth.renderNote(audioData, "f4", crotchet);
-  synth.renderNote(audioData, "e4", crotchet);
-  synth.renderNote(audioData, "d4", crotchet);
-  synth.renderNote(audioData, "c4", crotchet);
-  synth.renderNote(audioData, "c4", crotchet);
-  synth.renderNote(audioData, "d4", crotchet);
-  synth.renderNote(audioData, "e4", crotchet);
-  synth.renderNote(audioData, "e4", crotchet*1.5);
-  synth.renderNote(audioData, "d4", crotchet*0.5);
-  synth.renderNote(audioData, "d4", crotchet*2);
+  auto voices{bachPrelude(synth)};
+  auto combined{synth.combineVoices(voices)};
 
   WaveFile waveFile{sampleRate, numChannels, bitsPerSample};
-  waveFile.pushSamples(audioData);
+  waveFile.pushSamples(combined);
   waveFile.saveToFile("output.wav");
 
   return 0;
+}
+
+std::vector<std::vector<std::vector<double>>> bachPrelude(Synthesizer &synth)
+{
+  // Create a voice for each hand and get references to them
+  std::vector<std::vector<std::vector<double>>> voices(
+      2, std::vector<std::vector<double>>(
+             synth.getNumChannels(), std::vector<double>{}));
+
+  auto &v1{voices[0]};
+  auto &v2{voices[1]};
+
+  // Get length in seconds of a semiquaver
+  double tempo{120.0};
+  double crotchet{60.0 / tempo};
+  double semiquaver{crotchet / 4};
+
+  // Notes to Bach's Prelude in C Minor, BWV 847
+  // Bar 1
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "c5", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+    synth.renderNote(v1, "d4", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+    synth.renderNote(v1, "c4", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+    synth.renderNote(v1, "d4", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+
+    synth.renderNote(v2, "c3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "f3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "eb3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "f3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+  }
+  // Bar 2
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "ab4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "c4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+
+    synth.renderNote(v2, "c3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+    synth.renderNote(v2, "f3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+  }
+  // Bar 3
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "b4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "d4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+
+    synth.renderNote(v2, "c3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+    synth.renderNote(v2, "f3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+  }
+  // Bar 4
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "c5", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "f4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+
+    synth.renderNote(v2, "c3", semiquaver);
+    synth.renderNote(v2, "eb3", semiquaver);
+    synth.renderNote(v2, "d3", semiquaver);
+    synth.renderNote(v2, "eb3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "eb3", semiquaver);
+    synth.renderNote(v2, "d3", semiquaver);
+    synth.renderNote(v2, "eb3", semiquaver);
+  }
+  // Bar 5
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "eb5", semiquaver);
+    synth.renderNote(v1, "ab4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "ab4", semiquaver);
+    synth.renderNote(v1, "eb4", semiquaver);
+    synth.renderNote(v1, "ab4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "ab4", semiquaver);
+
+    synth.renderNote(v2, "c3", semiquaver);
+    synth.renderNote(v2, "c4", semiquaver);
+    synth.renderNote(v2, "bb3", semiquaver);
+    synth.renderNote(v2, "c4", semiquaver);
+    synth.renderNote(v2, "ab3", semiquaver);
+    synth.renderNote(v2, "c4", semiquaver);
+    synth.renderNote(v2, "bb3", semiquaver);
+    synth.renderNote(v2, "c4", semiquaver);
+  }
+  // Bar 6
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "d5", semiquaver);
+    synth.renderNote(v1, "f#4", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+    synth.renderNote(v1, "f#4", semiquaver);
+    synth.renderNote(v1, "d4", semiquaver);
+    synth.renderNote(v1, "f#4", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+    synth.renderNote(v1, "f#4", semiquaver);
+
+    synth.renderNote(v2, "c3", semiquaver);
+    synth.renderNote(v2, "a3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "a3", semiquaver);
+    synth.renderNote(v2, "f#3", semiquaver);
+    synth.renderNote(v2, "a3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "a3", semiquaver);
+  }
+  // Bar 7
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "d5", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "f#4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "d4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+    synth.renderNote(v1, "f#4", semiquaver);
+    synth.renderNote(v1, "g4", semiquaver);
+
+    synth.renderNote(v2, "bb2", semiquaver);
+    synth.renderNote(v2, "bb3", semiquaver);
+    synth.renderNote(v2, "a3", semiquaver);
+    synth.renderNote(v2, "bb3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "bb3", semiquaver);
+    synth.renderNote(v2, "a3", semiquaver);
+    synth.renderNote(v2, "bb3", semiquaver);
+  }
+  // Bar 8
+  for (int i{0}; i < 2; i++)
+  {
+    synth.renderNote(v1, "c5", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+    synth.renderNote(v1, "d4", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+    synth.renderNote(v1, "c4", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+    synth.renderNote(v1, "d4", semiquaver);
+    synth.renderNote(v1, "e4", semiquaver);
+
+    synth.renderNote(v2, "bb2", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "f3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "e3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+    synth.renderNote(v2, "f3", semiquaver);
+    synth.renderNote(v2, "g3", semiquaver);
+  }
+
+  return voices;
 }
